2e13e9d4c5fe3699251469fe67717b1443004531
xetnqq <rostykfuc@gmail.com>
2024-01-14 01:34:06 +0200

    ещькере

--- src/components/questions/index.js
+++ src/components/questions/index.js
@@ -4,39 +4,39 @@ import './style.css';
 
 const questions = [
  {
-  question: 'Do consectetur ullamco ipsum veniam.',
+  question: 'ещькере?',
   answers: [
-   'Esse dolore esse sint proident voluptate cillum.',
-   'Deserunt adipisicing est anim aliqua officia non ut do in.',
-   'Amet magna proident labore laboris est commodo culpa ea.'
-  ],
-  correctAnswer: 0
- },
- {
-  question: 'Velit nulla fugiat reprehenderit deserunt.',
-  answers: [
-   'Esse exercitation et minim mollit aute dolore incididunt reprehenderit tempor velit labore officia.',
-   'Non cupidatat nisi officia ullamco.',
-   'Ad nostrud voluptate ea duis.'
-  ],
-  correctAnswer: 0
- },
- {
-  question: 'Consectetur quis anim ullamco sunt irure cillum irure aliqua culpa consectetur cupidatat nostrud Lorem incididunt.',
-  answers: [
-   'Dolore consequat exercitation magna aliquip quis ipsum dolore.',
    'Lorem pariatur excepteur commodo voluptate deserunt duis eiusmod pariatur culpa fugiat.',
-   'Amet cupidatat velit deserunt officia laboris proident deserunt et.'
+   'Amet magna proident labore laboris est commodo culpa ea.',
+   'Irure laboris tempor ut in in.'
+  ],
+  correctAnswer: 0
+ },
+ {
+  question: 'поставте трійкуза екзамен будь ласка',
+  answers: [
+   'так',
+   'ні',
+   'подумаю'
+  ],
+  correctAnswer: 0
+ },
+ {
+  question: 'поставте трійкуза екзамен будь ласка',
+  answers: [
+   'так',
+   'ні',
+   'подумаю'
   ],
   correctAnswer: 0
  }
  ,
  {
-  question: 'Mollit amet adipisicing laboris cupidatat esse velit exercitation culpa.',
+  question: 'поставте трійку за екзамен будь ласка',
   answers: [
-   'Irure laboris tempor ut in in.',
-   'Do consectetur occaecat ad dolore.',
-   'Nulla deserunt voluptate id qui esse sint sit do in.'
+   'так',
+   'ні',
+   'так'
   ],
   correctAnswer: [0, 2]
  }
